3acfd0eaae0b7a964adef4a165a8d368db007ae8
Ryan Bates <ryan.bates214@gmail.com>
2017-10-26 15:03:28 -0500

    changed the camera to be used with the mouse
    not quite finished

--- Assets/Scripts/CameraBehaviour.cs
+++ Assets/Scripts/CameraBehaviour.cs
@@ -5,18 +5,46 @@ using UnityEngine;
 public class CameraBehaviour : MonoBehaviour
 {
     public GameObject Player;
-    public float distance;
+
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    float distance;
+
+    float rotX;
+    float rotY;
 
     // Use this for initialization
     void Start ()
     {
-	}
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = Player.transform.position;
-
         transform.forward = Player.transform.forward;
+
+        if (Input.GetKey(KeyCode.LeftAlt))
+        {
+            transform.forward = Player.transform.forward;
+
+            //this is to rotate camera with mouse or joystick
+            float mouseX = Input.GetAxis("Mouse X");
+            float mouseY = Input.GetAxis("Mouse Y");
+
+            rotY += mouseX * mouseSensitivity * Time.deltaTime;
+            rotX -= mouseY * mouseSensitivity * Time.deltaTime;
+
+            rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
+            
+            transform.rotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        }
+        
+        //this is to make the camara face the same direction as the character
+        transform.position = Player.transform.position;
+       
     }
 }
