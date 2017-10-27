5abbc742d4ee7a82f1b28438a9571fc5d236a654
Jeremy Barzas <jbarzas@gmail.com>
2017-10-27 12:34:43 -0500

    current progress

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float movementSpeed;    
+
+    float horizontal;
+    float vertical;
+
+    void Move()
+    {   
+        // Keyboard transform of player
+        horizontal = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime; ;      
+        
+        transform.Translate(horizontal, 0, vertical);
+
+        // Mouse rotate of player
+        float mouseX = Input.GetAxis("Mouse X");
+
+        transform.Rotate(0, mouseX, 0);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+}
