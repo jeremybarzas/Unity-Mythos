76b41564b5ee207efb9cbd8af68b2f9049fad1d1
Jeremy Barzas <jbarzas@gmail.com>
2017-10-27 11:35:30 -0500

    current version

---
+++ Assets/Scripts/CopyRotationBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyRotationBehaviour : MonoBehaviour
+{
+    public GameObject player;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //float MouseY = Input.GetAxis("MouseY");
+        transform.rotation = new Quaternion(transform.rotation.x, player.transform.rotation.y, transform.rotation.z, transform.rotation.w);
+	}
+}
