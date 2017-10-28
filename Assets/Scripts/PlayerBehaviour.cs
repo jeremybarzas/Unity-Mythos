37a265673b85655c8a82c36ee90b54e99286ba4f
Jeremy Barzas <jbarzas@gmail.com>
2017-10-27 19:47:31 -0500

    final commit

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -5,23 +5,30 @@ public class PlayerBehaviour : EntityBehaviour, IDamageable
 {
     public Weapon _weapon;    
     private WeaponBehaviour _weaponBehaviour;
-    public Animator _weaponAnimator;    
-    
+    public Animator _weaponAnimator;
+    public Rigidbody _rigidbody;
+    public AudioSource _audioSource;
     private void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
+        _rigidbody = GetComponent<Rigidbody>();
         _weaponBehaviour = GetComponentInChildren<WeaponBehaviour>(); //we only have one weapon
         _weaponBehaviour.SetWeapon(_weapon); //set the current weapon
+        
+        
     }
 
     private void Update()
     {
         if (Input.GetButtonDown("Fire1"))
             _weaponAnimator.SetTrigger("swing");
+        _weaponAnimator.SetFloat("speed", _rigidbody.velocity.magnitude);
     }
 
     public void TakeDamage(int amount)
     {
-        GetComponent<AudioSource>().Play();
-        config["Health"].Value -= amount;       
+        _audioSource.Play();
+        config["Health"].Value -= amount;
+        _weaponAnimator.SetInteger("health", config["Health"].Value);
     }
 }
